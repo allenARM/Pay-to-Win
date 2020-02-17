0956cdddcdce1bbef3ecbe6e234c9e015b158773
8989896533q <allenwem@gmail.com>
2020-02-16 23:57:32 -0800

    added lose screen, fixed buttons, added purchases and ads

---
+++ Assets/Scritps/AfterPurchaseAddWin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AfterPurchaseAddWin : MonoBehaviour
+{
+	TextMeshProUGUI text;
+	int temp; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+	public void PurchaseMade()
+	{
+		temp = int.Parse(text.text);
+		temp++;
+		text.text = temp.ToString();
+	}
+}
